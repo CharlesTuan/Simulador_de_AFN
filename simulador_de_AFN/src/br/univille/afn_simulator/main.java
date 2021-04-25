11e4b587537fcd80c5d50fa07dfbb4063313803c
charles <charles.tavares@valid.com>
2021-04-24 22:43:49 -0300

    ta na mão chefia

--- simulador_de_AFN/src/br/univille/afn_simulator/main.java
+++ simulador_de_AFN/src/br/univille/afn_simulator/main.java
@@ -1,7 +1,9 @@
 package br.univille.afn_simulator;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -10,18 +12,30 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class main {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 
-		Integer line = 0;
-		Scanner nodeRulesDataArchive = new Scanner(new FileReader("src/archive/dados.txt"));
+		//Faz a leitura do arquivo de regras do automato
+		Scanner nodeRulesDataArchive = new Scanner(new FileReader("src/archive/rules.txt"));
 
+		//Cria o arquivo de saída
+		FileWriter outPutArquive = new FileWriter("src/archive/outPut.txt");
+		PrintWriter writerOutPutArquive = new PrintWriter(outPutArquive);
+
+		//Lista dos nodes que serão conhecidos pelo automato
 		List<String> nodeArray = null;
+		//Dialeto conhecido pelo automato
 		List<String> dialect = null;
+		//Regras de saida para cada dialeto
 		List<String> rule = null;
 
+		//Mapa que garda a relação de node para dialeto e regra
 		Map<String, Map<String, List<String>>> nodeFullData = new HashMap<String, Map<String, List<String>>>();
+		//Mapa que garda a regra do node
 		Map<String, List<String>> nodeRule = new HashMap<String, List<String>>();
 
+		//Linha do arquivo sendo executado
+		Integer line = 0;
+		//Informação da linha sendo executada
 		String lineText = null;
 		Integer ruleLines = 0;
 		String initialState = null;
@@ -93,6 +107,8 @@ public class main {
 
 		List<String> nextNode;
 
+		List<String> epsomNextNode;
+
 		Integer commandSize = null;
 
 		String nextNodes = "";
@@ -101,9 +117,10 @@ public class main {
 
 			line++;
 
-			System.out.println("------------------------------");
 			System.out.println("Linha de execução -> " + line.toString());
 			System.out.println("Estado inicial ->  " + initialState);
+			writerOutPutArquive.printf("Linha de execução -> " + line.toString() + "\n");
+			writerOutPutArquive.printf("Estado inicial ->  " + initialState + "\n");
 
 			lineText = inputNodeAction.nextLine();
 
@@ -117,10 +134,15 @@ public class main {
 
 			List<Map<String, List<String>>> nodeRuleListAux = new ArrayList<Map<String, List<String>>>();
 
+			List<String> epsomNode;
+
+			Map<String, List<String>> epsomRules = new HashMap<String, List<String>>();
+
 			commandSize = 0;
 
 			for (String command : commandList) {
 				System.out.println("Simbolo lido ->  " + command);
+				writerOutPutArquive.printf("Simbolo lido ->  " + command + "\n");
 
 				commandSize++;
 
@@ -130,32 +152,53 @@ public class main {
 
 					nextNode = nodeRuleItem.get(command);
 
+					epsomNextNode = new ArrayList<String>();
+
+					epsomNextNode.addAll(nextNode);
+
 					for (String node : nextNode) {
+						epsomRules = nodeFullData.get(node);
+						if (epsomRules != null) {
+							epsomNode = epsomRules.get("Épsylon");
+
+							for (String epsonNodeItem : epsomNode) {
+								if (epsomNode != null && !epsonNodeItem.equals("*")) {
+									epsomNextNode.add(epsonNodeItem);
+								}
+							}
+						}
+					}
+
+					for (String node : epsomNextNode) {
 						nextNodes += node + " ";
 					}
 
 					System.out.println("Estados correntes -> " + nextNodes);
+					writerOutPutArquive.printf("Estados correntes -> " + nextNodes + "\n");
 
 					nextNodes = "";
 
-					for (String node : nextNode) {
+					for (String node : epsomNextNode) {
 						if (commandList.size() == commandSize) {
 							System.out.println("Estado de aceitação -> "
-									+ (nextNode.get(0).equals(finalState) ? "Aceito" : "Rejeitado"));
+									+ (epsomNextNode.get(0).equals(finalState) ? "Aceito" : "Rejeitado"));
+							writerOutPutArquive.printf("Estado de aceitação -> "
+									+ (epsomNextNode.get(0).equals(finalState) ? "Aceito" : "Rejeitado") + "\n");
 						}
 						nodeRule = nodeFullData.get(node);
 
-						if (nodeRule != null)
+						if (nodeRule != null) {
+
 							nodeRuleListAux.add(nodeRule);
-
+						} else if (commandList.size() != commandSize) {
+							System.out.println("Estado de aceitação -> Rejeitado");
+							writerOutPutArquive.printf("Estado de aceitação -> Rejeitado" + "\n");
+						}
 					}
-
 				}
-
 				nodeRuleList = nodeRuleListAux;
-
 			}
 		}
-
+		writerOutPutArquive.close();
 	}
 }
